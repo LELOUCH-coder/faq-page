d66b362bdd865e1b49a8897f17252593ae608bad
samish <155093381+LELOUCH-coder@users.noreply.github.com>
2024-04-02 20:36:51 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const questions=document.querySelectorAll(".questions");
+
+questions.forEach(function(question){
+	
+const btn=question.querySelector(".buttons");
+
+btn.addEventListener("click",function(){
+	
+	
+	
+questions.forEach(function(item){
+	
+	if(item!==question){
+		item.classList.remove("show-text");
+	}
+	
+	
+});
+	question.classList.toggle("show-text");
+});
+	
+});
